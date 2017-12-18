ccc1a3eef3a4e009c011f84527328fd4f8c53f90
Gaurav Lalchandani <glalchandanig@gmail.com>
2017-12-18 17:05:05 +0530

    Login page made(frontend only)

--- addon_files/index.js
+++ addon_files/index.js
@@ -3,9 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
 	var signup_btn = document.getElementById('signup_btn');
 	signup_btn.addEventListener('click', show_signup_page);
 
+	var login_btn = document.getElementById('login_btn');
+	login_btn.addEventListener('click', show_login_page);	
+
 });
 
 function show_signup_page(){
 	document.getElementById("homepage").style.display = "none";
+	document.getElementById("login_page").style.display = "none";
 	document.getElementById("signup_page").style.display = "block";
+}
+
+function show_login_page(){
+	document.getElementById("homepage").style.display = "none";
+	document.getElementById("signup_page").style.display = "none";
+	document.getElementById("login_page").style.display = "block";
 }
