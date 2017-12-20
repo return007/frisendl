98058206bebe2167f8da0139a4fb767b9b14cda4
Gaurav Lalchandani <glalchandanig@gmail.com>
2017-12-20 19:02:51 +0530

    main page structure and friend list display

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	var submit_login_btn = document.getElementById('submit_login_btn');
 	submit_login_btn.addEventListener('click', perform_login);
 
+	var friend_section_btn = document.getElementById('friend_section_btn');
+	friend_section_btn.addEventListener('click', show_friend_section);
+
+	var upload_section_btn = document.getElementById('upload_section_btn');
+	upload_section_btn.addEventListener('click', show_upload_section);
+
+	var go_btn = document.getElementById('go_btn');
+	go_btn.addEventListener('click', show_search_result);
+
 });
 
 function display_error_message(msg){
@@ -32,6 +41,7 @@ function hide_success_message(){
 function show_signup_page(){
 	document.getElementById("homepage").style.display = "none";
 	document.getElementById("login_page").style.display = "none";
+	document.getElementById("main_page").style.display = "none";	
 	document.getElementById("signup_page").style.display = "block";
 
 	hide_error_message();
@@ -44,8 +54,15 @@ function show_signup_page(){
 }
 
 function show_login_page(){
+
+	//For development ease only.... To be removed after development
+	show_main_page();
+	return;
+	//ends here </>
+
 	document.getElementById("homepage").style.display = "none";
 	document.getElementById("signup_page").style.display = "none";
+	document.getElementById("main_page").style.display = "none";	
 	document.getElementById("login_page").style.display = "block";
 
 	hide_error_message();
@@ -53,6 +70,70 @@ function show_login_page(){
 
 	document.getElementById("email").addEventListener('keypress', hide_error_message);
 	document.getElementById("password").addEventListener('keypress', hide_error_message);
+}
+
+function show_main_page(){
+	document.getElementById("homepage").style.display = "none";
+	document.getElementById("signup_page").style.display = "none";
+	document.getElementById("login_page").style.display = "none";
+	document.getElementById("main_page").style.display = "block";
+
+	hide_error_message();
+	hide_success_message();
+}
+
+function show_friend_section(){
+	document.getElementById("friend_section").style.display = "block";
+	document.getElementById("upload_section").style.display = "none";	
+}
+
+function show_upload_section(){
+	document.getElementById("upload_section").style.display = "block";
+	document.getElementById("friend_section").style.display = "none";	
+}
+
+function show_search_result(){
+
+	var search_query = document.getElementById("search_query").value;
+	if(is_empty(search_query)){
+		return;
+	}
+
+	var friend_list = document.getElementsByClassName("friend");
+	for(var itr=0; itr < friend_list.length; itr++){
+		friend_list[itr].style.display = "none";
+	}
+	document.getElementById("friend_list_header").innerHTML = "Search Results";
+
+	//Make a network request to perform search: based on the given text and return the result
+	//Make search_element class based divs to display inside the friend_list div
+	//search_element class div will have Name, Email ID (hidden characters too), FriendRequest Option (if not a friend)
+
+	var request_body = {
+		"q": search_query
+	};
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if (xhr.readyState == 4) {
+			if(xhr.status == 200){
+				var json_response = JSON.parse(xhr.responseText);
+				if(json_response.status == "OK"){
+					display_success_message(json_response.response);
+				}
+				else{
+					display_error_message(json_response.response);
+				}
+			}
+			else
+				display_error_message("Unsuccessful HTTP Request!");
+		}
+	};
+	xhr.open("POST", "http://localhost/cgi-bin/search_friend.py", true);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.send(JSON.stringify(request_body));	
+
 }
 
 function is_empty(val){
@@ -107,6 +188,7 @@ function perform_signup(){
 				var json_response = JSON.parse(xhr.responseText);
 				if(json_response.status == "OK"){
 					display_success_message("Successfully registered! Login to continue.");
+					setTimeout(show_login_page, 1000);
 				}
 				else{
 					display_error_message(json_response.response);
@@ -124,6 +206,7 @@ function perform_signup(){
 }
 
 function perform_login(){
+
 	var login = document.getElementById("login_page").children;
 	var email = login[0].value;
 	var password = login[2].value;
@@ -148,6 +231,7 @@ function perform_login(){
 				var json_response = JSON.parse(xhr.responseText);
 				if(json_response.status == "OK"){
 					display_success_message("Successful Login!");
+					setTimeout(show_main_page, 1500);
 				}
 				else{
 					display_error_message(json_response.response);
@@ -161,5 +245,4 @@ function perform_login(){
 	xhr.setRequestHeader("Accept", "application/json");
 	xhr.setRequestHeader("Content-Type", "application/json");
 	xhr.send(JSON.stringify(request_body));
-
 }
